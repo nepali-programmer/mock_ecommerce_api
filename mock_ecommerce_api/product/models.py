38cf9f0de4de2d3a95513a1cba25a30eea4ce34a
Sijan Joshi <94091594+nepali-programmer@users.noreply.github.com>
2023-05-29 06:55:01 +0545

    added product

--- mock_ecommerce_api/product/models.py
+++ mock_ecommerce_api/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    image = models.ImageField()
+    amount =  models.FloatField(blank=False, null=False)
+
+    def __str__(self):
+        return str(self.name)
